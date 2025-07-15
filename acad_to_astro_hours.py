5a4a9d96416c21c656d005a243cde5e14c0bbf70
Pavel2706115 <pavel.andreev05@gmail.com>
2025-07-15 10:05:53 +0300

    First task academic hours -> astro hours - GPT

--- acad_to_astro_hours.py
+++ acad_to_astro_hours.py
@@ -1,9 +1,16 @@
-astro = 60
-acad = 40
-session = 4*acad - 20
-if course is 64*session:
-    course_astro = course/astro
-
-print("Total duration in astronomical hours:", course_astro)
+#Input
+total_academic_hours = 64
+# Constants
+astronomical_hour_minutes = 60
+academical_hour_minutes = 40
+academical_hours_per_session = 4
+break_minutes_per_session = 20
+#Number of sessions
+num_sessions = total_academic_hours // academical_hours_per_session
+#Total time in minutes
+total_minutes = (total_academic_hours * academical_hour_minutes) + (num_sessions * break_minutes_per_session)
+# Conversion to astronomical hours
+total_astronomical_hours = total_minutes / astronomical_hour_minutes
+print("Total duration in astronomical hours:", total_astronomical_hours)
     
     
